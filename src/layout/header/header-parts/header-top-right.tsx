a28ff440c3da59eb5f29d94c1facbf4ec9d32a83
Volodymyr Makarovskyi <makarovskyi.v@gmail.com>
2025-02-02 18:44:41 +0200

    added html component

---
+++ src/layout/header/header-parts/header-top-right.tsx
@@ -0,0 +1,69 @@
+import React, { FC, useState } from "react";
+
+type SettingsAndLanguageProps = {
+  active: string;
+  onHandleActive: (type: string) => void;
+};
+
+const Language: FC<SettingsAndLanguageProps> = ({
+  active,
+  onHandleActive,
+}): JSX.Element => {
+  let className = 'header__top-right-item-parts'
+
+  return (
+    <li className="header__top-right-item">
+      <span className="header__top-right-item-label" onClick={() => onHandleActive('language')}>English</span>
+      <ul className={active === 'language' ? `${className} is-language-list-open` : `${className} d-none`}>
+        <li>Ukrainian</li>
+        <li>Russian</li>
+        <li>Spanish</li>
+      </ul>
+    </li>
+  );
+};
+
+const Settings: FC<SettingsAndLanguageProps> = ({
+  active,
+  onHandleActive,
+}): JSX.Element => {
+  let className = "header__top-right-item-parts";
+
+  return (
+    <li className="header__top-right-item">
+      <span
+        className="header__top-right-item"
+        onClick={() => onHandleActive("settings")}
+      >
+        Settings
+      </span>
+      <ul
+        className={
+          active === "settings" ? `${className} is-settings-list-open` : `${className} d-none`
+        }
+      >
+        <li>My profile</li>
+        <li>Login</li>
+      </ul>
+    </li>
+  );
+};
+
+export const HeaderTopRight: FC = (): JSX.Element => {
+  const [active, setActive] = useState("");
+
+  const handleActive = (type: string): void => {
+    if (active === type) {
+      setActive("");
+    } else {
+      setActive(type);
+    }
+  };
+
+  return (
+    <ul className="header__top-right-list d-flex justify-content-end">
+      <Language active={active} onHandleActive={handleActive} />
+      <Settings active={active} onHandleActive={handleActive} />
+    </ul>
+  );
+};
