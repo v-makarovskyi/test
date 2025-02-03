b474c0bd9d01acb5ee935bf430d2e95b333b1570
Volodymyr Makarovskyi <makarovskyi.v@gmail.com>
2025-02-03 15:57:19 +0200

    added menu-data

---
+++ src/data/menu-data.js
@@ -0,0 +1,56 @@
+export const menu_data = [
+  {
+    id: 1,
+    home: true,
+    title: "Популярное",
+    link: "/",
+  },
+  {
+    id: 2,
+    trends: true,
+    title: "Тренды",
+    link: "/trends",
+  },
+  {
+    id: 3,
+    guides: true,
+    title: "Туториалы",
+    link: "/guides",
+  },
+  {
+    id: 4,
+    advices: true,
+    title: "Советы",
+    link: "/advices",
+    big_menu: [
+      {
+        title: "Тренды",
+        link: "/trends",
+      },
+      {
+        title: "Туториалы",
+        link: "/guides",
+      },
+      {
+        title: "Get the look",
+        link: "/get_the_look",
+      },
+      {
+        title: "Контакты",
+        link: "/contacts",
+      },
+    ],
+  },
+  {
+    id: 5,
+    get_the_look: true,
+    title: "Get the look",
+    link: "/get_the_look",
+  },
+  {
+    id: 6,
+    conyacts: true,
+    title: "Контакты",
+    link: "/contacts",
+  },
+];
