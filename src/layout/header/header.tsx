da94fe9af5b4cce141b4a7d47707c36450eb07ad
Volodymyr Makarovskyi <makarovskyi.v@gmail.com>
2025-02-04 20:11:23 +0200

    updated html

--- src/layout/header/header.tsx
+++ src/layout/header/header.tsx
@@ -1,12 +1,19 @@
 import React, { FC } from "react";
+import logo from '../../../src/images/logos/logo-2.png';
 import { PhoneTwo } from "../../svg/phoneTwo";
 import { Facebook } from "../../svg/facebook";
+import { Wishlist } from "../../svg/wishlist";
+import { Hamburger } from "../../svg/hamburger";
+import { User } from "../../svg/user";
+import { Search } from "../../svg/search";
 import { HeaderTopRight } from "./header-parts/header-top-right";
+import { Menus } from "./header-parts/menus";
 
 export const Header: FC = (): JSX.Element => {
   return (
     <header className="header">
       <div className="header__inner">
+        {/*   HEADER TOP */}
         <div className="header__top d-none d-md-block">
           <div className="container">
             <div className="row align-items-center">
@@ -18,7 +25,7 @@ export const Header: FC = (): JSX.Element => {
                         <span className="visually-hidden">header info</span>
                         <Facebook />
                         <span className="header__top-info-part">
-                          6600K followers
+                          6600K Followers
                         </span>
                       </a>
                     </li>
@@ -47,7 +54,63 @@ export const Header: FC = (): JSX.Element => {
             </div>
           </div>
         </div>
-        <div className="header__bottom"></div>
+
+        {/*  HEADER BOTTOM */}
+        <div className="header__bottom">
+          <div className="container">
+            <div className="header__bottom-inner">
+               <div className="row align-items-center">
+              <div className="col-6 col-sm-4 col-md-5 col-lg-5 col-xl-2">
+                <a className="header__logo logo" href="/">
+                  <img
+                    className="logo__image"
+                    src={logo}
+                    alt=""
+                    width="500"
+                    height="178"
+                    loading="lazy"
+                  />
+                </a>
+              </div>
+              <div className="d-none d-xl-block col-xl-5">
+                <nav className="header__menu menus">
+                  <Menus />
+                </nav>
+              </div>
+              <div className="col-6 col-sm-8 col-md-7 col-lg-7 col-xl-5">
+                <div className="header__bottom-right d-flex align-items-center justify-content-end">
+                  <div className="header__search search d-none d-sm-block">
+                    <form className="search__form">
+                      <input className="search__input input" type="text" placeholder="Search for blogs..." />
+                      <button className="search__button button" type="submit">
+                        <Search />
+                      </button>
+                    </form>
+                  </div>
+                  <ul className="header__actions d-flex align-items-center">
+                    <li className="header__actions-item">
+                      <a className="header__actios-link" href="/">
+                        <User />
+                      </a>
+                    </li>
+                    <li className="header__actions-item d-none d-lg-block">
+                      <a className="header__actios-link" href="/">
+                        <Wishlist />
+                      </a>
+                    </li>
+                    <li className="header__actions-item d-block d-xl-none">
+                      <button className="header__actios-link header__hamburger-button button">
+                        <Hamburger />
+                      </button>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            </div>
+           
+          </div>
+        </div>
       </div>
     </header>
   );
