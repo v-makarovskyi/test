faef1b067bcde455f646d89ef802621177ec3431
Volodymyr Makarovskyi <makarovskyi.v@gmail.com>
2025-02-04 20:12:02 +0200

    added svg react Components

---
+++ src/svg/hamburger.tsx
@@ -0,0 +1,16 @@
+import React, {FC} from "react";
+
+export const Hamburger: FC = (): JSX.Element => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="30"
+      height="16"
+      viewBox="0 0 30 16"
+    >
+      <rect x="10" width="20" height="2" fill="currentColor" />
+      <rect x="5" y="7" width="25" height="2" fill="currentColor" />
+      <rect x="10" y="14" width="20" height="2" fill="currentColor" />
+    </svg>
+  );
+};
