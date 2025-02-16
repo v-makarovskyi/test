56105039128a9fe3b1b1bf42e5bfc1161052a49b
Volodymyr Makarovskyi <makarovskyi.v@gmail.com>
2025-02-16 18:23:49 +0200

    added html for register-area. Not register-form

--- src/components/login-register/register-area.tsx
+++ src/components/login-register/register-area.tsx
@@ -1,5 +1,30 @@
 import React, { FC } from "react";
 
 export const RegisterArea: FC = (): JSX.Element => {
-  return <div>RegisterArea</div>;
+  return (
+    <section className="login">
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-lg-8 col-xl-6">
+            <div className="login__wrapper">
+              <header className="login__header">
+                <h3 className="login__title">Зарегистрироваться в Ulta</h3>
+                <p className="login__description">
+                  Есть аккаунт?&nbsp;
+                  <span>
+                    <a href="/">Войти</a>
+                  </span>
+                </p>
+              </header>
+              <div className="login__body">
+                <div className="login__email">
+                  <p>или зарегистрируйтесь по Email</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
