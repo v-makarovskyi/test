e174f4d770fd2324b2b7e9ef13812f9d2202fcdf
Volodymyr Makarovskyi <makarovskyi.v@gmail.com>
2025-02-17 18:15:43 +0200

    create html & styles for LoginShapes component

--- src/components/login-register/login-shapes.tsx
+++ src/components/login-register/login-shapes.tsx
@@ -1,7 +1,14 @@
-import React, { FC } from 'react'
+import React, { FC } from "react";
+import shape_1 from "../../images/login-shapes/but-1.png";
+import shape_2 from "../../images/login-shapes/but-2.png";
+import shape_3 from "../../images/login-shapes/but-3.png";
 
-export const LoginShapes:FC = ():JSX.Element => {
+export const LoginShapes: FC = (): JSX.Element => {
   return (
-    <div> LoginShapes</div>
-  )
-}
+    <div className="login__shapes">
+      <img className="login__shapes-1" src={shape_1} alt="" loading="lazy" />
+      <img className="login__shapes-2" src={shape_2} alt="" loading="lazy" />
+      <img className="login__shapes-3" src={shape_3} alt="" loading="lazy" />
+    </div>
+  );
+};
