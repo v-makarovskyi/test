15e8c3f2c08d5e692afbddbeec75a3e36005be8a
Volodymyr Makarovskyi <makarovskyi.v@gmail.com>
2025-02-17 16:36:49 +0200

    updated classes

--- src/components/forms/register-form.tsx
+++ src/components/forms/register-form.tsx
@@ -7,66 +7,60 @@ export const RegisterForm: FC = (): JSX.Element => {
     <form className="login__form" id="login-form">
       <div className="login__form-input-box-wrapper">
         <div className="login__form-input-box">
-          <div className="login__form-field">
-            <input
-              className="login__form-input input"
-              id="name"
-              name="name"
-              placeholder="Vladimir Makarovskyi"
-              type="text"
-            />
-          </div>
-          <div className="login__form-title">
-            <label htmlFor="name">Your name</label>
-          </div>
+          <input
+            className="login__form-input input"
+            id="name"
+            name="name"
+            placeholder="Vladimir Makarovskyi"
+            type="text"
+          />
+          <label className="login__form-title" htmlFor="name">
+            Your name
+          </label>
         </div>
         <div className="login__form-input-box">
-          <div className="login__form-field">
-            <input
-              className="login__form-input  input"
-              id="email"
-              name="email"
-              placeholder="makarovskyi.v@gmail.com"
-              type="email"
-            />
-          </div>
-          <div className="login__form-title">
-            <label htmlFor="name">Your email</label>
-          </div>
+          <input
+            className="login__form-input  input"
+            id="email"
+            name="email"
+            placeholder="makarovskyi.v@gmail.com"
+            type="email"
+          />
+          <label className="login__form-title" htmlFor="email">
+            Your email
+          </label>
         </div>
-        <div className="login__form-input-box login__form-input-box--relative ">
-          <div className="login__form-field">
-            <input
-              className="login__form-input input"
-              id="password"
-              name="password"
-              placeholder="min 6 character"
-              type="password"
-            />
-          </div>
-          <div className="login__form__eye" id="password-show-toggle">
+        <div className="login__form-input-box">
+          <input
+            className="login__form-input input"
+            id="password"
+            name="password"
+            placeholder="min 6 character"
+            type="password"
+          />
+          <div className="login__form-eye" id="password-show-toggle">
             <span className="login__form-open-eye">
               <CloseEye />
             </span>
           </div>
-          <div className="login__form-title">
-            <label htmlFor="name">Enter password</label>
-          </div>
+          <label className="login__form-title" htmlFor="password">
+            Enter password
+          </label>
         </div>
       </div>
-      <fieldset className="login__remember">
+      <fieldset className="login__form-remember" id="login-form">
         <legend className="visually-hidden">
           accept the terms of the Service{" "}
         </legend>
         <label htmlFor="remember">
           <input
-            className="login__remember-checkbox visually-hidden"
+            className="login__form-remember-checkbox visually-hidden"
             type="checkbox"
             id="remember"
             name="remember"
           />
-          <span className="login__emulator"></span>
-          <span className="login__remember-title">
+          <span className="login__form-remember-emulator"></span>
+          <span className="login__form-remember-title">
             I accept the terms of the Service & <a href="/">Privacy Policy</a> .
           </span>
         </label>
